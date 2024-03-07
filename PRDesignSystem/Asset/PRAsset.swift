ac70a27c29f6041717f12399441c8fc152d4aa86
Taeyoung Won <hexter1994@gmail.com>
2024-03-07 16:30:17 +0900

    [Feat] PR 컬러셋 추가

---
+++ PRDesignSystem/Asset/PRAsset.swift
@@ -0,0 +1,20 @@
+//
+//  PRAsset.swift
+//  PRDesignSystem
+//
+//  Created by 원태영 on 3/7/24.
+//
+
+import UIKit
+
+public enum PRAsset {
+  
+  public enum Color {
+    
+    public static let prPrimary: UIColor = UIColor(hex: "#1A3568")
+    public static let prSecondary: UIColor = UIColor(hex: "#E2EDF6")
+    public static let prTitle: UIColor = UIColor(hex: "#222A3A")
+    public static let prMainInfo: UIColor = UIColor(hex: "#214C9E")
+    public static let prSubInfo: UIColor = UIColor(hex: "#606673")
+  }
+}
