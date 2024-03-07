e05b8aa553ef64f5830152738feec2fab70c033b
Taeyoung Won <hexter1994@gmail.com>
2024-03-07 16:28:23 +0900

    [Build] PRDesignSystem 프레임워크 타겟 추가

---
+++ PRDesignSystem/PRDesignSystem.h
@@ -0,0 +1,18 @@
+//
+//  PRDesignSystem.h
+//  PRDesignSystem
+//
+//  Created by 원태영 on 3/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PRDesignSystem.
+FOUNDATION_EXPORT double PRDesignSystemVersionNumber;
+
+//! Project version string for PRDesignSystem.
+FOUNDATION_EXPORT const unsigned char PRDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PRDesignSystem/PublicHeader.h>
+
+
