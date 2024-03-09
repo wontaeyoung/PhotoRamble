fdf9ed3b80073b4e660a1922736723dc7e9b47fa
Taeyoung Won <hexter1994@gmail.com>
2024-03-09 23:27:57 +0900

    [Feat] LocationDTO 모델 정의

---
+++ PhotoRamble/Data/DTO/LocationDTO.swift
@@ -0,0 +1,27 @@
+//
+//  LocationDTO.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/9/24.
+//
+
+import Foundation
+import KazRealm
+import RealmSwift
+
+final class LocationDTO: Object, RealmModel, DTO {
+  
+  enum Column: String {
+    
+    case id
+    case latitude
+    case longitude
+    case timestamp
+  }
+  
+  @Persisted(primaryKey: true) var id: UUID
+  @Persisted var latitude: Double
+  @Persisted var longitude: Double
+  @Persisted var timestamp: Date
+}
+
