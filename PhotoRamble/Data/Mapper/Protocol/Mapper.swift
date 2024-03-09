52d44baeb4cf74106917c9cf5e2c096b6abccdb6
Taeyoung Won <hexter1994@gmail.com>
2024-03-10 00:09:55 +0900

    [Feat] Mapper 프로토콜 정의

---
+++ PhotoRamble/Data/Mapper/Protocol/Mapper.swift
@@ -0,0 +1,14 @@
+//
+//  Mapper.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/9/24.
+//
+
+protocol Mapper {
+  associatedtype DTOType: DTO
+  associatedtype EntityType: Entity
+  
+  static func toDTO(_ entity: EntityType) throws -> DTOType
+  static func toEntity(_ dto: DTOType) throws -> EntityType
+}
