d70cf721d6f9902b39d220fd40f7214c509caff1
Taeyoung Won <hexter1994@gmail.com>
2024-03-10 19:02:41 +0900

    [Feat] Post Mapper 객체 구현

---
+++ PhotoRamble/Data/Mapper/PostMapper.swift
@@ -0,0 +1,35 @@
+//
+//  PostMapper.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/10/24.
+//
+
+import RealmSwift
+
+struct PostMapper: Mapper {
+  
+  static func toDTO(_ entity: Post) -> PostDTO{
+    return PostDTO(
+      id: entity.id,
+      content: entity.content,
+      createAt: entity.createAt,
+      updateAt: entity.updateAt,
+      writingStatus: entity.writingStatus.rawValue,
+      photoIndices: entity.photoIndices.toList,
+      walkID: entity.walkID
+    )
+  }
+  
+  static func toEntity(_ dto: PostDTO) -> Post {
+    return Post(
+      id: dto.id,
+      content: dto.content,
+      createAt: dto.createAt,
+      updateAt: dto.updateAt,
+      writingStatus: .init(from: dto.writingStatus),
+      photoIndices: dto.photoIndices.toArray,
+      walkID: dto.walkID
+    )
+  }
+}
