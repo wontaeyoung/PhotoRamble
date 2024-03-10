1d4a58ddd7b7bab220724f0cbc2083a9ec9a000e
Taeyoung Won <hexter1994@gmail.com>
2024-03-10 19:02:18 +0900

    [Feat] Location List <-> Array 변환 로직 추가 구현

--- PhotoRamble/Data/Mapper/LocationMapper.swift
+++ PhotoRamble/Data/Mapper/LocationMapper.swift
@@ -5,26 +5,37 @@
 //  Created by 원태영 on 3/9/24.
 //
 
-import Foundation
 import RealmSwift
 
 struct LocationMapper: Mapper {
   
-  static func toDTO(_ entity: Location) throws -> LocationDTO {
+  static func toDTO(_ entity: Location) -> LocationDTO {
     return LocationDTO(
-      id: try ObjectId(string: entity.id.uuidString),
+      id: entity.id,
       latitude: entity.latitude,
       longitude: entity.longitude,
       timestamp: entity.timestamp
     )
   }
   
-  static func toEntity(_ dto: LocationDTO) throws -> Location {
+  static func toDTO(_ entities: [Location]) -> List<LocationDTO> {
+    return entities
+      .map { toDTO($0) }
+      .toList
+  }
+  
+  static func toEntity(_ dto: LocationDTO) -> Location {
     return Location(
-      id: try UUID.from(string: dto.id.stringValue),
+      id: dto.id,
       latitude: dto.latitude,
       longitude: dto.longitude,
       timestamp: dto.timestamp
     )
+  }
+  
+  static func toEntity(_ dtos: List<LocationDTO>) -> [Location] {
+    return dtos
+      .toArray
+      .map { toEntity($0) }
   }
 }
