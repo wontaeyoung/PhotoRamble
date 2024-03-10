ef8e074d83bf3bdc9e7030b032c650c969cb852e
Taeyoung Won <hexter1994@gmail.com>
2024-03-10 19:01:18 +0900

    [Feat] Array 변환 익스텐션 구현

---
+++ PhotoRamble/Utility/Extension/List+.swift
@@ -0,0 +1,15 @@
+//
+//  List+.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/10/24.
+//
+
+import RealmSwift
+
+extension List {
+  
+  var toArray: Array<Element> {
+    return Array(self)
+  }
+}
