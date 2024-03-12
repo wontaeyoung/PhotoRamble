de700d47d323f856255ca1d1012373b4f456d832
Taeyoung Won <hexter1994@gmail.com>
2024-03-12 22:24:48 +0900

    [Feat] 글로벌 네비게이션 바 색상 설정

--- PhotoRamble/App/AppDelegate.swift
+++ PhotoRamble/App/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
     connectFirebase()
+    setGlobalNavigationBarAppearance()
     
     return true
   }
@@ -24,5 +25,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   private func connectFirebase() {
     FirebaseApp.configure()
+  }
+  
+  private func setGlobalNavigationBarAppearance() {
+    
+    UINavigationBar.appearance().configure {
+      let appearance = UINavigationBarAppearance().configured {
+        $0.backgroundColor = PRAsset.Color.prPrimary
+        $0.titleTextAttributes = [.foregroundColor:PRAsset.Color.prWhite]
+        $0.largeTitleTextAttributes = [.foregroundColor:PRAsset.Color.prWhite]
+      }
+      
+      $0.tintColor = PRAsset.Color.prWhite
+      $0.barTintColor = PRAsset.Color.prWhite
+      $0.standardAppearance = appearance
+      $0.compactAppearance = appearance
+      $0.scrollEdgeAppearance = appearance
+      $0.compactScrollEdgeAppearance = appearance
+    }
   }
 }
