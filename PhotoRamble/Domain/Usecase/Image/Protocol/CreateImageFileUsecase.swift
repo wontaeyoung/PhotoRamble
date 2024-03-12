91760903326b99f6660b5e6f63bed7cc8f88b384
Taeyoung Won <hexter1994@gmail.com>
2024-03-12 13:36:22 +0900

    [Feat] 이미지 생성 Usecase 인터페이스 정의

---
+++ PhotoRamble/Domain/Usecase/Image/Protocol/CreateImageFileUsecase.swift
@@ -0,0 +1,14 @@
+//
+//  CreateImageFileUsecase.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/11/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol CreateImageFileUsecase {
+  
+  func excute(imageData: Data, directoryName: String, fileIndex: Int) -> Single<Data>
+}
