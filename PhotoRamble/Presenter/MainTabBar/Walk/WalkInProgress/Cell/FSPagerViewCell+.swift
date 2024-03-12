e206fa929c02e47bd013d6ae45e3dc0eeeb0cdc1
Taeyoung Won <hexter1994@gmail.com>
2024-03-12 17:45:21 +0900

    [Feat] identifier 추가

---
+++ PhotoRamble/Presenter/MainTabBar/Walk/WalkInProgress/Cell/FSPagerViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  FSPagerViewCell+.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/11/24.
+//
+
+import FSPagerView
+
+extension FSPagerViewCell {
+  class var identifier: String {
+    return self.description()
+  }
+}
