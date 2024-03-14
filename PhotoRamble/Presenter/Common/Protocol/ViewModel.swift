4ddfd37ed834817fe2ac606482ff2747edb6aae4
Taeyoung Won <hexter1994@gmail.com>
2024-03-14 09:00:51 +0900

    [Feat] coordinator 프로퍼티 메서드 정의

--- PhotoRamble/Presenter/Common/Protocol/ViewModel.swift
+++ PhotoRamble/Presenter/Common/Protocol/ViewModel.swift
@@ -11,10 +11,20 @@ protocol ViewModel: AnyObject {
   
   associatedtype Input
   associatedtype Output
+  associatedtype CoordinatorType: Coordinator
   
   // MARK: - Property
   var disposeBag: DisposeBag { get }
+  var coordinator: CoordinatorType? { get set }
   
   // MARK: - Method
   func transform(input: Input) -> Output
+}
+
+extension ViewModel {
+  func coordinator(_ coordinator: CoordinatorType) -> Self {
+    self.coordinator = coordinator
+    
+    return self
+  }
 }
