c477b3e5009c2d6998d5db8a90ea7804a2c3f7be
Taeyoung Won <hexter1994@gmail.com>
2024-03-14 18:58:38 +0900

    [Add] 토스트 메세지 배경색 추가

--- PhotoRamble/DesignSystem/Asset/PRAsset.swift
+++ PhotoRamble/DesignSystem/Asset/PRAsset.swift
@@ -19,6 +19,7 @@ public enum PRAsset {
     public static let prPlaceholder: UIColor = UIColor(hex: "#A9A9A9")
     public static let prWhite: UIColor = UIColor(hex: "#FFFFFF")
     public static let prBlack: UIColor = UIColor(hex: "#000000")
+    public static let prToastBacgrkound: UIColor = UIColor(hex: "F3F4F6")
   }
   
   public enum Font {
