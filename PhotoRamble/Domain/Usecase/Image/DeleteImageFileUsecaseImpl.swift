ef4e82c7e6a31d1633d7d21de991044ff8f9ca26
Taeyoung Won <hexter1994@gmail.com>
2024-03-15 16:10:22 +0900

    [Feat] 이미지 전체 삭제 Usecase 구현

---
+++ PhotoRamble/Domain/Usecase/Image/DeleteImageFileUsecaseImpl.swift
@@ -0,0 +1,25 @@
+//
+//  DeleteImageFileUsecaseImpl.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/15/24.
+//
+
+import Foundation
+import RxSwift
+
+final class DeleteImageFileUsecaseImpl: DeleteImageFileUsecase {
+  
+  // MARK: - Property
+  private let imageRepository: ImageRepository
+  
+  // MARK: - Initializer
+  init(imageRepository: some ImageRepository) {
+    self.imageRepository = imageRepository
+  }
+  
+  // MARK: - Method
+  func excute(directoryName: String) -> Single<Void> {
+    return imageRepository.deleteAll(directoryName: directoryName)
+  }
+}
