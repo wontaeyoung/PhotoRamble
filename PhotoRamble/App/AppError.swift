1500ec629ecdf1e84ca051b7f8c710f4a58a31e6
Taeyoung Won <hexter1994@gmail.com>
2024-03-15 16:01:24 +0900

    [Add] nil 발생 에러 케이스 추가

--- PhotoRamble/App/AppError.swift
+++ PhotoRamble/App/AppError.swift
@@ -13,19 +13,22 @@ public protocol AppError: Error {
 public enum CommonError: AppError {
   
   case unknownError(error: Error?)
+  case unExceptedNil
   
   public var logDescription: String {
     switch self {
       case .unknownError(let error):
         return "알 수 없는 오류 발생 \(error?.localizedDescription ?? "")"
+        
+      case .unExceptedNil:
+        return "Nil 객체 발생"
     }
   }
   
   public var alertDescription: String {
     switch self {
-      case .unknownError:
-        return "알 수 없는 오류가 발생했어요. 문제가 지속되면 개발자에게 알려주세요."
+      case .unknownError, .unExceptedNil:
+        return "알 수 없는 문제가 발생했어요. 문제가 지속되면 개발자에게 알려주세요."
     }
   }
 }
-
