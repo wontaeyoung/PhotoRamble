8417167b702aace7d598e80c1d7d0ff26fa70f41
Taeyoung Won <hexter1994@gmail.com>
2024-03-16 12:33:41 +0900

    [Feat] 가로 스크롤 스타일 섹션 추가

--- PhotoRamble/Utility/Extension/UI/NSCollectionLayoutSection+.swift
+++ PhotoRamble/Utility/Extension/UI/NSCollectionLayoutSection+.swift
@@ -163,4 +163,46 @@ public extension NSCollectionLayoutSection {
     
     return section
   }
+  
+  static func makeHorizontalScrollSection(
+    itemSpacing: Double = 0,
+    sectionInset: NSDirectionalEdgeInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0),
+    header: Bool = false,
+    headerHeight: CGFloat = 30
+  ) -> NSCollectionLayoutSection {
+    
+    let item: NSCollectionLayoutItem = NSCollectionLayoutItem(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(0.3),
+        heightDimension: .fractionalHeight(1)
+      )
+    )
+    
+    let group: NSCollectionLayoutGroup = .horizontal(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1),
+        heightDimension: .fractionalHeight(1)
+      ),
+      subitems: [item]
+    )
+    
+    group.interItemSpacing = .fixed(itemSpacing)
+    
+    let section = NSCollectionLayoutSection(group: group).configured {
+      $0.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
+      $0.contentInsets = sectionInset
+    }
+    
+    if header {
+      section.boundarySupplementaryItems = [
+        NSCollectionLayoutBoundarySupplementaryItem(
+          layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(headerHeight)),
+          elementKind: UICollectionView.elementKindSectionHeader,
+          alignment: .top
+        )
+      ]
+    }
+    
+    return section
+  }
 }
