4324de320b3eeb65aab643cd41c00c5bac52093e
Taeyoung Won <hexter1994@gmail.com>
2024-03-19 16:07:50 +0900

    [Add] 산책 데이터 생성 에러 케이스 추가

--- PhotoRamble/App/PRError.swift
+++ PhotoRamble/App/PRError.swift
@@ -34,4 +34,23 @@ enum PRError {
       }
     }
   }
+  
+  enum WalkRepository: AppError {
+    
+    case createFailed(error: Error)
+    
+    var logDescription: String {
+      switch self {
+        case .createFailed(let error):
+          return "산책 인스턴스 저장 실패. \(error.localizedDescription)"
+      }
+    }
+    
+    var alertDescription: String {
+      switch self {
+        case .createFailed:
+          return "산책 내용을 저장하는데 실패했어요. 문제가 지속되면 개발자에게 알려주세요."
+      }
+    }
+  }
 }
