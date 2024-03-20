4c8ef0615587411232ba0df70f16ec9b0a9eceb9
Taeyoung Won <hexter1994@gmail.com>
2024-03-20 15:42:57 +0900

    [Feat] 일기 리스트 컬렉션 레이아웃 구성, 일기 조회 결과 바인딩

--- PhotoRamble/Presenter/MainTabBar/Diary/DiaryListViewController.swift
+++ PhotoRamble/Presenter/MainTabBar/Diary/DiaryListViewController.swift
@@ -13,10 +13,15 @@ import RxCocoa
 final class DiaryListViewController: RXBaseViewController, ViewModelController {
   
   // MARK: - UI
+  private lazy var diaryListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
   
+  private let layout: UICollectionViewCompositionalLayout = .list(
+    using: UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+  )
   
   // MARK: - Property
   let viewModel: DiaryListViewModel
+  private var dataSource: UICollectionViewDiffableDataSource<Section, Diary>!
   
   // MARK: - Initializer
   init(viewModel: DiaryListViewModel) {
@@ -27,21 +32,67 @@ final class DiaryListViewController: RXBaseViewController, ViewModelController {
   
   // MARK: - Life Cycle
   override func setHierarchy() {
-    
+    view.addSubviews(diaryListCollectionView)
   }
   
   override func setConstraint() {
-    
+    diaryListCollectionView.snp.makeConstraints { make in
+      make.edges.equalTo(view.safeAreaLayoutGuide)
+    }
   }
   
   override func setAttribute() {
-    
+    setDataSource()
   }
   
   override func bind() {
+    let input = DiaryListViewModel.Input(viewDidLoadEvent: PublishRelay())
+    let output = viewModel.transform(input: input)
     
+    output.diaries
+      .drive(with: self) { owner, diaries in
+        owner.updateSnapshot(diaries: diaries)
+      }
+      .disposed(by: disposeBag)
+    
+    input.viewDidLoadEvent.accept(())
   }
   
   // MARK: - Method
   
+}
+
+// MARK: - Collection Configuration
+extension DiaryListViewController {
+  
+  enum Section: CaseIterable {
+    case main
+  }
+  
+  private func setDataSource() {
+    let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Diary> { cell, indexPath, item in
+      
+      cell.contentConfiguration = UIListContentConfiguration.cell().applied {
+        $0.text = item.content
+        $0.secondaryText = DateManager.shared.toString(with: item.createAt, format: .yyyyMMdd)
+      }
+    }
+    
+    dataSource = UICollectionViewDiffableDataSource(collectionView: diaryListCollectionView) { collectionView, indexPath, item in
+      return collectionView.dequeueConfiguredReusableCell(
+        using: cellRegistration,
+        for: indexPath,
+        item: item
+      )
+    }
+  }
+  
+  private func updateSnapshot(diaries: [Diary]) {
+    let snapshot = NSDiffableDataSourceSnapshot<Section, Diary>().applied {
+      $0.appendSections(Section.allCases)
+      $0.appendItems(diaries)
+    }
+    
+    dataSource.apply(snapshot)
+  }
 }
