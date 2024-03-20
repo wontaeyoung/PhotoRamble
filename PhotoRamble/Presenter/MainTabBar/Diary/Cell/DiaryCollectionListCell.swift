ebe43030771bf355feeedb9032f7d66aa3f05939
Taeyoung Won <hexter1994@gmail.com>
2024-03-20 16:04:55 +0900

    [Add] DiaryCollectionListCell 추가

---
+++ PhotoRamble/Presenter/MainTabBar/Diary/Cell/DiaryCollectionListCell.swift
@@ -0,0 +1,29 @@
+//
+//  DiaryCollectionListCell.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/20/24.
+//
+
+import UIKit
+import SnapKit
+import RxCocoa
+
+final class DiaryCollectionListCell: RXBaseCollectionViewListCell {
+  
+  // MARK: - UI
+  
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    
+  }
+  
+  override func setConstraint() {
+    
+  }
+  
+  override func setAttribute() {
+    
+  }
+}
