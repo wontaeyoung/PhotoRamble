317856cfe2c928b155d1a03cfb5758e0ea888983
Taeyoung Won <hexter1994@gmail.com>
2024-03-20 12:37:01 +0900

    [Add] DiaryListViewModel 클래스 추가

---
+++ PhotoRamble/Presenter/MainTabBar/Diary/DiaryListViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  DiaryListViewModel.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/20/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class DiaryListViewModel: ViewModel {
+  
+  // MARK: - I / O
+  struct Input {
+    
+  }
+  
+  struct Output {
+    
+  }
+  
+  // MARK: - Property
+  let disposeBag = DisposeBag()
+  weak var coordinator: DiaryCoordinator?
+  
+  // MARK: - Initializer
+  init() {
+    
+  }
+  
+  // MARK: - Method
+  func transform(input: Input) -> Output {
+    
+    return Output()
+  }
+}
