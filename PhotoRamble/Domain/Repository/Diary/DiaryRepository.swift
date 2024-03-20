7f7da64ca8d761aee05c1a76d8cfd393795a5c5e
Taeyoung Won <hexter1994@gmail.com>
2024-03-20 09:43:11 +0900

    [Add] 일기 레포지토리 인터페이스 프로토콜 추가

---
+++ PhotoRamble/Domain/Repository/Diary/DiaryRepository.swift
@@ -0,0 +1,14 @@
+//
+//  DiaryRepository.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/20/24.
+//
+
+import RxSwift
+
+protocol DiaryRepository {
+  
+  func create(with diary: Diary) -> Single<Diary>
+}
+
