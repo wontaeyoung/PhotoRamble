3eb78e64d6137ee60b6be1db1ba69166b184505e
Taeyoung Won <hexter1994@gmail.com>
2024-03-20 09:43:26 +0900

    [Feat] 일기 레포지토리 구현체 클래스 작성

---
+++ PhotoRamble/Data/Repository/Diary/DiaryRepositoryImpl.swift
@@ -0,0 +1,35 @@
+//
+//  DiaryRepositoryImpl.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/20/24.
+//
+
+import Foundation
+import KazRealm
+import RealmSwift
+import RxSwift
+
+final class DiaryRepositoryImpl: DiaryRepository {
+  
+  private let service: any RealmService
+  private let mapper: DiaryMapper
+  
+  init(service: RealmService, mapper: DiaryMapper) {
+    self.service = service
+    self.mapper = mapper
+  }
+  
+  func create(with diary: Diary) -> Single<Diary> {
+    
+    let diaryDTO = mapper.toDTO(diary)
+    
+    do {
+      try service.create(with: diaryDTO)
+      return .just(diary)
+    } catch {
+      return .error(PRError.RealmRepository.createFailed(error: error, modelName: "일기"))
+    }
+  }
+}
+
