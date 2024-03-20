433546954284e62ce94ca99f85d22c522c0f9638
Taeyoung Won <hexter1994@gmail.com>
2024-03-20 15:42:37 +0900

    [Feat] 일기 조회 이벤트 바인딩 구현

--- PhotoRamble/Presenter/MainTabBar/Diary/DiaryListViewModel.swift
+++ PhotoRamble/Presenter/MainTabBar/Diary/DiaryListViewModel.swift
@@ -12,25 +12,42 @@ final class DiaryListViewModel: ViewModel {
   
   // MARK: - I / O
   struct Input {
-    
+    let viewDidLoadEvent: PublishRelay<Void>
   }
   
   struct Output {
-    
+    let diaries: Driver<[Diary]>
   }
+  
+  // MARK: - Observable
+  private let diariesRelay = BehaviorRelay<[Diary]>(value: [])
   
   // MARK: - Property
   let disposeBag = DisposeBag()
   weak var coordinator: DiaryCoordinator?
+  private let fetchDiaryUsecase: any FetchDiaryUsecase
   
   // MARK: - Initializer
-  init() {
-    
+  init(fetchDiaryUsecase: some FetchDiaryUsecase) {
+    self.fetchDiaryUsecase = fetchDiaryUsecase
   }
   
   // MARK: - Method
   func transform(input: Input) -> Output {
     
-    return Output()
+    input.viewDidLoadEvent
+      .withUnretained(self)
+      .flatMap { owner, _ in
+        return owner.fetchDiaryUsecase.execute()
+          .asObservable()
+      }
+      .subscribe(with: self) { owner, diaries in
+        owner.diariesRelay.accept(diaries)
+      }
+      .disposed(by: disposeBag)
+    
+    return Output(
+      diaries: diariesRelay.asDriver()
+    )
   }
 }
