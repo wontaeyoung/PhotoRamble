e2887368dfcce3cb3b514ff2b9f76d2422613ec7
Taeyoung Won <hexter1994@gmail.com>
2024-03-20 16:04:40 +0900

    [Feat] Base 컬렉션 리스트 셀 클래스 추가

---
+++ PhotoRamble/Presenter/Common/Base/RXBaseListCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  RXBaseCollectionViewListCell.swift
+//  PhotoRamble
+//
+//  Created by 원태영 on 3/20/24.
+//
+
+import UIKit
+import RxSwift
+
+open class RXBaseCollectionViewListCell: UICollectionViewListCell {
+  
+  open class var identifier: String {
+    return self.description()
+  }
+  
+  // MARK: - Property
+  public var disposeBag = DisposeBag()
+  
+  // MARK: - Initializer
+  public override init(frame: CGRect) {
+    
+    super.init(frame: frame)
+    
+    backgroundColor = .clear
+    contentView.backgroundColor = .clear
+    
+    setHierarchy()
+    setConstraint()
+    setAttribute()
+  }
+  
+  @available(*, unavailable)
+  public required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycle
+  open func setHierarchy() { }
+  open func setConstraint() { }
+  open func setAttribute() { }
+}
