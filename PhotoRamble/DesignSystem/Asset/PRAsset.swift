0ccfc609554356a58f57b1e36c3b6c968b38cb5b
Taeyoung Won <hexter1994@gmail.com>
2024-03-22 17:06:28 +0900

    [Add] 배경색 추가

--- PhotoRamble/DesignSystem/Asset/PRAsset.swift
+++ PhotoRamble/DesignSystem/Asset/PRAsset.swift
@@ -13,6 +13,7 @@ public enum PRAsset {
     
     public static let prPrimary: UIColor = UIColor(hex: "#1A3568")
     public static let prSecondary: UIColor = UIColor(hex: "#E2EDF6")
+    public static let prBackground: UIColor = UIColor(hex: "#F4FBFD")
     public static let prTitle: UIColor = UIColor(hex: "#222A3A")
     public static let prMainInfo: UIColor = UIColor(hex: "#214C9E")
     public static let prSubInfo: UIColor = UIColor(hex: "#606673")
