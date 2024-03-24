1def4d6be116487711062414928675d9bda04054
Taeyoung Won <hexter1994@gmail.com>
2024-03-25 01:38:11 +0900

    [Feat] 일기 셀 탭 이벤트와 화면 이동 로직 연결

--- PhotoRamble/Presenter/MainTabBar/Diary/DiaryListViewModel.swift
+++ PhotoRamble/Presenter/MainTabBar/Diary/DiaryListViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 원태영 on 3/20/24.
 //
 
+import Foundation
 import RxSwift
 import RxCocoa
 
@@ -13,6 +14,7 @@ final class DiaryListViewModel: ViewModel {
   // MARK: - I / O
   struct Input {
     let viewDidLoadEvent: PublishRelay<Void>
+    let diaryCellTapEvent: PublishRelay<IndexPath>
   }
   
   struct Output {
@@ -26,10 +28,15 @@ final class DiaryListViewModel: ViewModel {
   let disposeBag = DisposeBag()
   weak var coordinator: DiaryCoordinator?
   private let fetchDiaryUsecase: any FetchDiaryUsecase
+  private let fetchWalkUsecase: any FetchWalkUsecase
   
   // MARK: - Initializer
-  init(fetchDiaryUsecase: some FetchDiaryUsecase) {
+  init(
+    fetchDiaryUsecase: some FetchDiaryUsecase,
+    fetchWalkUsecase: some FetchWalkUsecase
+  ) {
     self.fetchDiaryUsecase = fetchDiaryUsecase
+    self.fetchWalkUsecase = fetchWalkUsecase
   }
   
   // MARK: - Method
@@ -46,6 +53,24 @@ final class DiaryListViewModel: ViewModel {
       }
       .disposed(by: disposeBag)
     
+    input.diaryCellTapEvent
+      .map { self.diary(at: $0) }
+      .withUnretained(self)
+      .flatMap { owner, diary in
+        return owner.fetchWalkUsecase.execute(walkID: diary.walkID)
+          .asObservable()
+          .map { ($0, diary) }
+      }
+      .subscribe(with: self) { owner, result in
+        let (walk, diary) = result
+        
+        owner.coordinator?.showDiaryDetailView(diary: diary, walk: walk)
+      } onError: { owner, error in
+        LogManager.shared.log(with: error, to: .local)
+        owner.coordinator?.showErrorAlert(error: error)
+      }
+      .disposed(by: disposeBag)
+
     BindingContainer.shared.diaryTableUpdatedEvent
       .bind(to: input.viewDidLoadEvent)
       .disposed(by: disposeBag)
@@ -53,5 +78,9 @@ final class DiaryListViewModel: ViewModel {
     return Output(
       diaries: diariesRelay.asDriver()
     )
+  }
+  
+  private func diary(at indexPath: IndexPath) -> Diary {
+    return diariesRelay.value[indexPath.item]
   }
 }
