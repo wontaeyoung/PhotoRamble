593fb32c4a4801b95f3249b4fc53c8c809f12f7c
Taeyoung Won <hexter1994@gmail.com>
2024-03-25 01:34:30 +0900

    [Feat] 모델 데이터 UI 바인딩 로직 적용

--- PhotoRamble/Presenter/MainTabBar/Diary/DiaryDetail/DiaryDetailViewController.swift
+++ PhotoRamble/Presenter/MainTabBar/Diary/DiaryDetail/DiaryDetailViewController.swift
@@ -19,7 +19,6 @@ final class DiaryDetailViewController: RXBaseViewController, ViewModelController
   private let contentView = UIView()
   
   private lazy var photoPagerView = FSPagerView().configured {
-    $0.isInfinite = true
     $0.delegate = self
     $0.dataSource = self
     $0.register(
@@ -28,8 +27,7 @@ final class DiaryDetailViewController: RXBaseViewController, ViewModelController
     )
   }
   
-  private let pageControl = FSPageControl().configured {
-    $0.numberOfPages = 3
+  private lazy var pageControl = FSPageControl().configured {
     $0.contentHorizontalAlignment = .center
     $0.backgroundColor = PRAsset.Color.prBlack.withAlphaComponent(0.05)
   }
@@ -52,6 +50,7 @@ final class DiaryDetailViewController: RXBaseViewController, ViewModelController
   
   // MARK: - Property
   let viewModel: DiaryDetailViewModel
+  private let photoPagerRelay = BehaviorRelay<[UIImage]>(value: [])
   
   // MARK: - Initializer
   init(viewModel: DiaryDetailViewModel) {
@@ -90,7 +89,7 @@ final class DiaryDetailViewController: RXBaseViewController, ViewModelController
     photoPagerView.snp.makeConstraints { make in
       make.top.equalTo(contentView.safeAreaLayoutGuide)
       make.horizontalEdges.equalTo(contentView)
-      make.height.equalTo(photoPagerView.snp.width)
+      make.height.equalTo(photoPagerView.snp.width).multipliedBy(0.75)
     }
     
     pageControl.snp.makeConstraints { make in
@@ -129,37 +128,59 @@ final class DiaryDetailViewController: RXBaseViewController, ViewModelController
     }
   }
   
-  override func setAttribute() {
-    
-  }
-  
   override func bind() {
-    dateLabel.text = "2024년 11월 25일 금요일"
-    walkTimeLabel.text = "01시간 25분 32초"
-    contentLabel.text = """
-등 날리라 당번이, 계좌의 삼고 해소하다. 공약의 역시 이라 법률적 당면하다. 기술자다 보통은 나가다 민주화는 기근이 못한 대통령으로 터무니없다. 고려하다 개설에 선수의 시든다면 기법의 의하게 않아야, 변화를 있다. "데 대국을 회의는 촉구한, 세탁에, 알려지다 조직화도 계속 마음먹지 폭등하다" 기능공으로 감소는 장비가 앞길의 지방화를 가다. 공휴일이 장려한 기념사업이, 한창은 사고의 또는 필요한 동원으로 청산이 통하다.
-
-부동산으로 오아 있고 동호회가 없애는 심리다 예비군에 교수의 시월일 정성이라고 어떻다. 기술은 당선되기, 중 사실이다 연초는 하다. 책의 서류에서 불로를 지지를 변화에 이상 된다. 여의 있다, 회장이 의미는 퇴임하다. "회사에 재활용품에서 개국, 발생량과 받아들이고자 어림잡다" 돋우어야 기지에 물러난지 조사는 하다. 투숙을 수익성으로 22일 교수 투입하다. 용감하고 것 한 군사력이 법인 변주곡은 모으고 아니라. 장관과 뒷받침되는 누구는 있은 한 중 사정이 30일, 보도한, 아니다.
-
-반군과 이제 것 입건할 이면까지 처분하다. "독재로 권총인 발표로 싶어 하다" 최초와 생활을 정계를 지원만 및 아래로 없어라. 자유조차 7,980,000원 하게 협력을 수작으로 대선을 증가하고 벌지 비서관은 나선다 포함하다. 하늘빛의 기술은 강북부터 이후도 2024년 피해의, 위한다. 근절하는 추첨을 의거하여야 우리를 유행하라.
-"""
+    let input = DiaryDetailViewModel.Input()
+    let output = viewModel.transform(input: input)
+    
+    output.dateText
+      .drive(dateLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    output.walkTimeText
+      .drive(walkTimeLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    output.contentText
+      .drive(contentLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    output.imageDataList
+      .flatMap {
+        Observable.from($0)
+          .compactMap { UIImage(data: $0) }
+          .toArray()
+          .asDriver(onErrorJustReturn: [])
+      }
+      .drive(photoPagerRelay)
+      .disposed(by: disposeBag)
   }
 }
 
 extension DiaryDetailViewController: FSPagerViewDelegate, FSPagerViewDataSource {
   func numberOfItems(in pagerView: FSPagerView) -> Int {
-    return 3
+    let numberOfItems = viewModel.numberOfItems
+    updatePagerConfiguration(numberOfItems: numberOfItems)
+    
+    return numberOfItems
   }
   
   func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
     let cell = pagerView.dequeueReusableCell(withReuseIdentifier: FSPagerViewCell.identifier, at: index)
     
-    cell.imageView?.image = UIImage(systemName: "person.fill")
+    cell.imageView?.configure {
+      $0.image = photoPagerRelay.value[index]
+      $0.contentMode = .scaleAspectFill
+    }
     
     return cell
   }
   
   func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
     pageControl.currentPage = targetIndex
+  }
+  
+  private func updatePagerConfiguration(numberOfItems: Int) {
+    pageControl.numberOfPages = numberOfItems
+    photoPagerView.isInfinite = numberOfItems > 1
   }
 }
--- PhotoRamble/Presenter/MainTabBar/Diary/DiaryDetail/DiaryDetailViewModel.swift
+++ PhotoRamble/Presenter/MainTabBar/Diary/DiaryDetail/DiaryDetailViewModel.swift
@@ -7,6 +7,7 @@
 
 import RxSwift
 import RxCocoa
+import Foundation
 
 final class DiaryDetailViewModel: ViewModel {
   
@@ -16,20 +17,64 @@ final class DiaryDetailViewModel: ViewModel {
   }
   
   struct Output {
-    
+    let dateText: Driver<String>
+    let walkTimeText: Driver<String>
+    let contentText: Driver<String>
+    let imageDataList: Driver<[Data]>
   }
+  
+  // MARK: - Observable
+  private let diary: BehaviorRelay<Diary>
+  private let walk: BehaviorRelay<Walk>
+  private let imageDataList: BehaviorRelay<[Data]>
   
   // MARK: - Property
   let disposeBag = DisposeBag()
   weak var coordinator: DiaryCoordinator?
+  private let fetchImageUsecase: any FetchImageFileUsecase
+  
+  var numberOfItems: Int {
+    return imageDataList.value.count
+  }
   
   // MARK: - Initializer
-  init() {
-    
+  init(diary: Diary, walk: Walk, fetchImageUsecase: some FetchImageFileUsecase) {
+    self.imageDataList = BehaviorRelay(value: [])
+    self.diary = BehaviorRelay(value: diary)
+    self.walk = BehaviorRelay(value: walk)
+    self.fetchImageUsecase = fetchImageUsecase
   }
   
   // MARK: - Method
   func transform(input: Input) -> Output {
     
+    fetchImageUsecase.execute(directoryName: walk.value.id.uuidString)
+      .asDriver(onErrorJustReturn: [])
+      .drive(imageDataList)
+      .disposed(by: disposeBag)
+    
+    let dateText = Observable.just(walkDateString(date: walk.value.startAt))
+      .asDriver(onErrorJustReturn: "-")
+    
+    let walkTimeText = Observable.just(walkTimeString(duration: walk.value.walkDuration))
+      .asDriver(onErrorJustReturn: DateManager.shared.elapsedTime(0, format: .HHmmssKR))
+    
+    let contentText = Observable.just(diary.value.content)
+      .asDriver(onErrorJustReturn: "")
+    
+    return Output(
+      dateText: dateText,
+      walkTimeText: walkTimeText,
+      contentText: contentText,
+      imageDataList: imageDataList.asDriver()
+    )
+  }
+  
+  private func walkDateString(date: Date) -> String {
+    return DateManager.shared.toString(with: date, format: .yyyyMMddEEEEKR)
+  }
+  
+  private func walkTimeString(duration: Int) -> String {
+    return DateManager.shared.elapsedTime(duration, format: .HHmmssKR)
   }
 }
