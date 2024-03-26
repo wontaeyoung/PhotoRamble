152029db59624942492713000360b0a26de2e811
Taeyoung Won <hexter1994@gmail.com>
2024-03-26 15:50:06 +0900

    [Feat] Usecase 의존성 추가, 앱 문서 웹뷰 연결

--- PhotoRamble/Presenter/MainTabBar/Setting/Coordinator/SettingCoordinator.swift
+++ PhotoRamble/Presenter/MainTabBar/Setting/Coordinator/SettingCoordinator.swift
@@ -29,21 +29,36 @@ extension SettingCoordinator {
   
   func showSettingView() {
     
+    let service = LiveRealmService()
+    let mapper = DiaryMapper()
+    
     let appInfoRepository = AppInfoRepositoryImpl()
     let appPermissionRepository = AppPermissionRepositoryImpl()
+    let diaryRepository = DiaryRepositoryImpl(service: service, mapper: mapper)
     
     let fetchAppVersionUsecase = FetchAppVersionUsecaseImpl(appInfoRepository: appInfoRepository)
     let canAccessCameraUsecase = CanAccessCameraUsecaseImpl(appPermissionRepository: appPermissionRepository)
+    let deleteAllDiaryUsecase = DeleteAllDiaryUsecaseImpl(diaryRepository: diaryRepository)
     
     let viewModel = SettingViewModel(
       fetchAppVersionUsecase: fetchAppVersionUsecase,
-      canAccessCameraUsecase: canAccessCameraUsecase
+      canAccessCameraUsecase: canAccessCameraUsecase,
+      deleteAllDiaryUsecase: deleteAllDiaryUsecase
     )
       .coordinator(self)
     
     let viewController = SettingViewController(viewModel: viewModel)
       .hideBackTitle()
       .navigationTitle(with: Localization.tab_setting.localized, displayMode: .never)
+    
+    push(viewController)
+  }
+  
+  func showSettingWebView(webCase: SettingWebCase) {
+    
+    let viewController = SettingWebViewController(webCase: webCase)
+      .hideBackTitle()
+      .navigationTitle(with: webCase.navigationTitle, displayMode: .never)
     
     push(viewController)
   }
